5f469101ea4a129a8fa21db0249e52ca0eec69bf
SyedFarooqHassan789 <farooqhassan92@hotmail.com>
2018-05-17 16:27:06 +0300

    Input is added

---
+++ CoverterApp.py
@@ -0,0 +1,5 @@
+class ConverterApp:
+    def main(self):
+        filename = input('Select 1 for Moarse to English and Select 2 for English to Moarse')
+        message = "GEEKS-FOR-GEEKS"
+        result = encrypt(message.upper())
