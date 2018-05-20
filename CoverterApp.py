46f6a2d209f4118061bde02a566f14850d40b429
SyedFarooqHassan789 <farooqhassan92@hotmail.com>
2018-05-20 14:16:40 +0300

    added input and output files

--- CoverterApp.py
+++ CoverterApp.py
@@ -8,14 +8,55 @@ class ConverterApp:
         self.getMorseToEnglish = MorseToEnglish.MorseConverter()
 
     def main(self):
-        filename = input('Select 1 for Morse to English and Select 2 for English to Morse')
-        morseValue = "--. . . -.- ... -....- ..-. --- .-. -....- --. . . -.- ..."
-        englishWord = "GEEKS-FOR-GEEKS"
-        self.getMorseToEnglish.morseValue = morseValue
-        result = self.getMorseToEnglish.getMorseToEnglish()
+        while True:
+            try:
+                selection = int(input('Select 1 for Morse to English and Select 2 for English to Morse\n'))
+            except ValueError:
+                print("Wrong Input")
+                continue
+            else:
+                break
 
-        self.getEnglishToMorse.englishWord = englishWord
-        result = self.getEnglishToMorse.getEnglishToMorse()
+        self.addingInputFile(selection)
+
+    def addingInputFile(self, selection):
+        inputFileValue = ''
+        try:
+            inputFile = input('Enter File Name For Input\n')
+            with open(inputFile, 'r') as inputFile:
+                inputFileValue = inputFile.read()
+
+            result = self.handlingSelection(selection, inputFileValue)
+            if result is not 0:
+                self.writeToOutputFile(result)
+        except FileNotFoundError:
+            pass
+        except IOError:
+            pass
+
+    def handlingSelection(self, selection, inputValue):
+        if selection is 1:
+            self.getMorseToEnglish.morseValue = inputValue
+            result = self.getMorseToEnglish.getMorseToEnglish()
+        elif selection is 2:
+            self.getEnglishToMorse.englishWord = inputValue
+            result = self.getEnglishToMorse.getEnglishToMorse()
+        else:
+            return 0
+        return result
+
+    def writeToOutputFile(self, result):
+        try:
+            outputFile = input('Enter File Name For Ouput\n')
+            with open(outputFile, 'w') as outputFile:
+                outputFile.write(result)
+        except IOError:
+            pass
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            print(e)
+            pass
 
 
 converterApp = ConverterApp()
